325451ca3c855b6003e6fe99ae37823b63afeef7
aimamovic5 <44173127+aimamovic5@users.noreply.github.com>
2018-10-29 04:49:07 +0100

    Dodao atribute klase i konstruktor

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,18 @@
 package ba.unsa.etf.rpr.tutorijal02;
-
+import java.lang.*;
 public class Interval {
+    private double pocetnaTacka;
+    private double krajnjaTacka;
+    private boolean pocetnaUkljucena;
+    private boolean krajnjaUkljucena;
+
+    public Interval(double pocetnaTacka, double krajnjaTacka, boolean pocetnaUkljucena, boolean krajnjaUkljucena) {
+        if (pocetnaTacka > krajnjaTacka) throw new IllegalArgumentException ("Greška");
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pocetnaUkljucena = pocetnaUkljucena;
+        this.krajnjaUkljucena = krajnjaUkljucena;
+    }
 
 
 }
