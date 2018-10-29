6a95e4928a315a69c41a95fac2cd0760307e5b29
aimamovic5 <44173127+aimamovic5@users.noreply.github.com>
2018-10-29 04:50:15 +0100

    Dodao getere, setere i konstruktor bez parametara

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -14,5 +14,55 @@ public class Interval {
         this.krajnjaUkljucena = krajnjaUkljucena;
     }
 
+    public double getPocetnaTacka() {
+        return pocetnaTacka;
+    }
+
+    public void setPocetnaTacka(double pocetnaTacka) {
+        this.pocetnaTacka = pocetnaTacka;
+    }
+
+    public double getKrajnjaTacka() {
+        return krajnjaTacka;
+    }
+
+    public void setKrajnjaTacka(double krajnjaTacka) {
+        this.krajnjaTacka = krajnjaTacka;
+    }
+
+    public boolean isPocetnaUkljucena() {
+        return pocetnaUkljucena;
+    }
+
+    public void setPocetnaUkljucena(boolean pocetnaUkljucena) {
+        this.pocetnaUkljucena = pocetnaUkljucena;
+    }
+
+    public boolean isKrajnjaUkljucena() {
+        return krajnjaUkljucena;
+    }
+
+    public void setKrajnjaUkljucena(boolean krajnjaUkljucena) {
+        this.krajnjaUkljucena = krajnjaUkljucena;
+    }
+
+    public Interval() {
+        this.pocetnaTacka = 0;
+        this.krajnjaTacka = 0;
+        this.pocetnaUkljucena = false;
+        this.krajnjaUkljucena = false;
+    }
+
+    public boolean isNull() {
+        return (Math.abs(pocetnaTacka - krajnjaTacka) < 0.00001);
+    }
+
+    public boolean isIn(double Tacka) {
+        if (Math.abs(Tacka - pocetnaTacka) < 0.00001 && this.isPocetnaUkljucena())
+            return true;
+        if (Math.abs(Tacka - krajnjaTacka) < 0.00001 && this.isKrajnjaUkljucena())
+            return true;
+        return (Tacka > pocetnaTacka && Tacka < krajnjaTacka);
+    }
 
 }
